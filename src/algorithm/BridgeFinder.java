2600e6383500944a4bb40e90eac1923979b1c7c8
ZhenjaMax <54914800+ZhenjaMax@users.noreply.github.com>
2021-07-05 16:34:06 +0200

    commit algorithm v1

---
+++ src/algorithm/BridgeFinder.java
@@ -0,0 +1,74 @@
+//package algorithm;
+//import Graph;
+//import Node;
+//import Edge;
+
+// Этот файл содержит void main,
+// необходимый для демонстрации работы
+// кода и тестирования классов 
+// Node, Edge, Graph.
+
+// Программа предлагает пользователю
+// ввести данные, удалить ненужные,
+// узнать промежуточные данные и
+// результат работы алгоритма Тарьяна.
+
+import java.util.Scanner;
+
+public class BridgeFinder{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int nodeCount, edgeCount;
+        Graph graph = new Graph();
+        String firstNodeName, secondNodeName;
+
+        System.out.printf("Write node count:\t");
+        nodeCount = in.nextInt();
+        for(int i = 0; i < nodeCount; i++){
+            firstNodeName = in.next();
+            if(!graph.addNode(firstNodeName))
+                System.out.printf("false!\n");
+        }
+        graph.print();
+        System.out.printf("=========\n\n");
+
+        System.out.printf("Write edge count:\t");
+        edgeCount = in.nextInt();
+        for(int i = 0; i < edgeCount; i++){
+            firstNodeName = in.next();
+            secondNodeName = in.next();
+            if(!graph.addEdge(firstNodeName, secondNodeName))
+                System.out.printf("false!\n");
+        }
+        graph.print();
+        System.out.printf("=========\n\n");
+
+        System.out.printf("Write remove node count:\t");
+        nodeCount = in.nextInt();
+        for(int i = 0; i < nodeCount; i++){
+            firstNodeName = in.next();
+            if(!graph.removeNode(firstNodeName))
+                System.out.printf("false!\n");
+        }
+        graph.print();
+        System.out.printf("=========\n\n");
+
+        System.out.printf("Write remove edge count:\t");
+        edgeCount = in.nextInt();
+        for(int i = 0; i < edgeCount; i++){
+            firstNodeName = in.next();
+            secondNodeName = in.next();
+            if(!graph.removeEdge(firstNodeName, secondNodeName))
+                System.out.printf("false!\n");
+        }
+        graph.print();
+        System.out.printf("=========\n\n");
+        
+        System.out.printf("Graph bridges finder:\n");
+        graph.runAlgorithm();
+        graph.print();
+
+        in.close();
+        return;
+    }
+}
---
+++ src/algorithm/Edge.java
@@ -0,0 +1,38 @@
+//package algorithm;
+//import Node;
+
+public class Edge {
+    private Node firstNode = null, secondNode = null;
+    public boolean isBridge = false;
+    public boolean isForward = false;
+    public boolean isBackward = false;
+
+    public Edge(Node firstNode, Node secondNode){
+        this.firstNode = firstNode;
+        this.secondNode = secondNode;
+    }
+    
+    public Node getFirstNode(){ return firstNode; }
+
+    public Node getSecondNode(){ return secondNode; }
+
+    public void swapNodes(){
+        Node tempNode = firstNode;
+        firstNode = secondNode;
+        secondNode = tempNode;
+    }
+
+    public Node getAdjacentNode(Node node){ return (firstNode == node) ? secondNode : firstNode; }
+
+    public boolean equals(Edge anotherEdge){
+        boolean isEqualForward = ((this.firstNode == anotherEdge.firstNode) && (this.secondNode == anotherEdge.secondNode));
+        boolean isEqualBackward = ((this.firstNode == anotherEdge.secondNode) && (this.secondNode == anotherEdge.firstNode));
+        return (isEqualForward || isEqualBackward);
+    }
+
+    public void reset(){
+        isBridge = false;
+        isForward = false;
+        isBackward = false;
+    }
+}
